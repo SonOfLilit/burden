06721dca39c071cbc1a26a98523b58402cdbde43
Aur Saraf <sonoflilit@gmail.com>
2013-02-16 17:18:58 +0200

    first app is included, updates to manage.py and README explain

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
+    BASE = os.path.abspath(os.path.dirname(__file__))
+    sys.path.append(os.path.join(BASE, "local_apps"))
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projects.burden.settings")
 
     from django.core.management import execute_from_command_line
